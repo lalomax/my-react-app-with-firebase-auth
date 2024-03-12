b1d86b40178fb6e72734b999839423984b533ef0
Lalomax <lalomax365@gmail.com>
2024-03-12 10:54:12 -0400

    Adding photo to profile

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -5,6 +5,7 @@ import { signOut } from "firebase/auth";
 const Profile = () => {
     const navigate = useNavigate();
     const user = auth.currentUser;
+    console.log(user)
 
     const logoutUser = async (e) => {
         e.preventDefault();
@@ -18,6 +19,7 @@ const Profile = () => {
             <div className = "row justify-content-center">
                 <div className = "col-md-4 text-center">
                     <p>Welcome <em className = "text-decoration-underline">{ user.email }</em>. You are logged in!</p>
+                    <img src={user.photoURL} alt="" />
                     <div className = "d-grid gap-2">
                         <button type = "submit" className = "text-white bg-violet-600 px-4 rounded-md" onClick = {(e) => logoutUser(e)}>Logout</button>
                     </div>                
